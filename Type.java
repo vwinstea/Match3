11e4c03c42712c730c69173c7b04de6c257376d9
Vance Winstead <32073638+vwinstea@users.noreply.github.com>
2019-10-10 11:04:46 -0700

    Delete Type.java

--- Type.java
+++
@@ -1,9 +0,0 @@
-/* Type.java - different types for different pieces
- * Author: Vance Winstead
- * Module: final projet
- */
-
-public enum Type {
-	DIAMOND, SQUARE, CIRCLE, STAR, TRISTAR, DROP, RING
-	//unicode: 074, 06E, 06C, 0AB, 0A9, 053, 0A3
-}
